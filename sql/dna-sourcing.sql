d9ed40b14aa5760a49206e73a75c0561c0e051a4
leewi9MBP <2768881880@qq.com>
2019-06-26 15:49:27 +0800

    minor fix

--- sql/dna-sourcing.sql
+++ sql/dna-sourcing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 172.17.0.1:3306
--- Generation Time: Jun 25, 2019 at 08:01 AM
+-- Generation Time: Jun 26, 2019 at 07:49 AM
 -- Server version: 5.7.26-0ubuntu0.16.04.1
 -- PHP Version: 7.2.14
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `tbl_action_dnaid`
 --
-drop table if exists `tbl_action_dnaid`;
+
 CREATE TABLE `tbl_action_dnaid` (
   `id` int(11) NOT NULL,
   `action_index` int(11) DEFAULT NULL,
@@ -44,9 +44,31 @@ CREATE TABLE `tbl_action_dnaid` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_contract`
+--
+
+CREATE TABLE `tbl_contract` (
+  `id` int(11) NOT NULL,
+  `dnaid` varchar(255) NOT NULL,
+  `company_dnaid` varchar(255) NOT NULL,
+  `txhash` varchar(255) NOT NULL,
+  `filehash` varchar(255) NOT NULL,
+  `detail` text NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `timestamp_sign` text NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 - 未删除；1 - 已删除；',
+  `revoke_tx` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_contract_company`
 --
-drop table if exists `tbl_contract_company`;
+
 CREATE TABLE `tbl_contract_company` (
   `id` int(11) NOT NULL,
   `dnaid` varchar(128) NOT NULL,
@@ -59,35 +81,9 @@ CREATE TABLE `tbl_contract_company` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_contract_dnaid`
---
-drop table if exists `tbl_contract_dnaid`;
-CREATE TABLE `tbl_contract_dnaid` (
-  `id` int(11) NOT NULL,
-  `dnaid` varchar(255) DEFAULT NULL,
-  `contract_index` int(11) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_contract_index`
---
-drop table if exists `tbl_contract_index`;
-CREATE TABLE `tbl_contract_index` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `flag` int(11) NOT NULL DEFAULT '0' COMMENT '当前操作表'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_event`
 --
-drop table if exists `tbl_event`;
+
 CREATE TABLE `tbl_event` (
   `id` int(11) NOT NULL,
   `txhash` varchar(255) NOT NULL,
@@ -105,7 +101,19 @@ CREATE TABLE `tbl_event` (
 -- Indexes for table `tbl_action_dnaid`
 --
 ALTER TABLE `tbl_action_dnaid`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `tbl_contract`
+--
+ALTER TABLE `tbl_contract`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `txhash` (`txhash`) USING BTREE,
+  ADD KEY `filehash` (`filehash`),
+  ADD KEY `dnaid` (`dnaid`),
+  ADD KEY `company_dnaid` (`company_dnaid`),
+  ADD KEY `type` (`type`);
 
 --
 -- Indexes for table `tbl_contract_company`
@@ -113,18 +121,6 @@ ALTER TABLE `tbl_action_dnaid`
 ALTER TABLE `tbl_contract_company`
   ADD PRIMARY KEY (`id`),
   ADD KEY `dnaid` (`dnaid`);
-
---
--- Indexes for table `tbl_contract_dnaid`
---
-ALTER TABLE `tbl_contract_dnaid`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tbl_contract_index`
---
-ALTER TABLE `tbl_contract_index`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_event`
@@ -144,21 +140,15 @@ ALTER TABLE `tbl_action_dnaid`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_contract`
+--
+ALTER TABLE `tbl_contract`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_contract_company`
 --
 ALTER TABLE `tbl_contract_company`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_contract_dnaid`
---
-ALTER TABLE `tbl_contract_dnaid`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_contract_index`
---
-ALTER TABLE `tbl_contract_index`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -171,6 +161,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-ALTER TABLE `tbl_action_dnaid` ADD INDEX(`username`);
